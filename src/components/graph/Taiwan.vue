d789bf74621f9b954ec9d8eff41be7e2835a3a12
as6325400 <as6325400@gmail.com>
2023-12-17 20:34:53 +0800

    return button

--- src/components/graph/Taiwan.vue
+++ src/components/graph/Taiwan.vue
@@ -1,4 +1,5 @@
 <template>
+  <ArrowLeftOutlined class="text-red-200" v-show="!TaiwanShow" @click="previous()"/>
   <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="100%" height="100%" viewBox="0 0 619 855" xml:space="preserve">
     <path
       v-for="pathData in TaiwanData"
@@ -26,20 +27,28 @@
 import Taiwan from "@/assets/map/Taiwan.json"
 import City from "@/assets/map/City.json"
 import { ref } from "vue"
+import { ArrowLeftOutlined } from '@ant-design/icons-vue';
 
 // 整個台灣的地圖資料，區域是一塊一塊的
 const TaiwanData = ref(Taiwan);
-
 const CityData = ref(City);
 
 const TaiwanShow = ref(true);
+
+// 紀錄現在選了哪個區域，全台灣時設為 -1 
+let RegionalIndex = -1;
 const RegionalShow = ref(new Array(5).fill(false));
 const CityShow = ref(new Array(22).fill(true));
 
 
+// 要 RegionalShow 跟 CityShow 都為 true 才能跑
+// 因此二級區域選單為選定區域 true 其餘 false
+// 三級城市預設全開啟
+
 function toRegional(id) {
   TaiwanShow.value = false;
   RegionalShow.value[id] = true;
+  RegionalIndex = id;
 }
 
 function toCity(id) {
@@ -48,4 +57,22 @@ function toCity(id) {
     if(i != id) CityShow.value[i] = false;
   }
 }
+
+function previous() {
+  let flag = false;
+
+  for(let i = 0; i < CityShow.value.length; i++){
+    if(CityShow.value[i] == false) flag = true;
+    CityShow.value[i] = true;
+  }
+
+  //  CityShow 都為 True ， 代表現在在二級選單
+  if(!flag){
+    TaiwanShow.value = true;
+    RegionalShow.value[RegionalIndex] = false;
+    RegionalIndex = -1;
+  }
+
+}
+
 </script>
--- src/main.js
+++ src/main.js
@@ -2,9 +2,10 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import './css/index.css'
+import Antd from 'ant-design-vue';
 
 const app = createApp(App)
 
 app.use(router)
-
+app.use(Antd)
 app.mount('#app')
